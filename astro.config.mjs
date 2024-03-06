a57a4db00fc7d4c292328c6528579fc16cdfdb3e
Alan-Kuan <kuanhaoan@gmail.com>
2024-03-07 00:22:28 +0800

    chore: add unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
 
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [ vue() ],
+  integrations: [
+    vue(),
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
   site: 'https://Alan-Kuan.github.io',
 });
