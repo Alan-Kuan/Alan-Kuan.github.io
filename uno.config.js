345313eb9b44971c114ff10baaad9e4a5dd5ad8d
Alan-Kuan <kuanhaoan@gmail.com>
2024-03-07 22:49:20 +0800

    feat: add my color scheme

--- uno.config.js
+++ uno.config.js
@@ -7,4 +7,22 @@ export default defineConfig({
     presetIcons(),
     presetUno(),
   ],
+  theme: {
+    colors: {
+      light: {
+        bg: '#fee0a8',
+        primary: '#74482d',
+        secondary: '#2b8ebb',
+        accent: '#fda229',
+        text: '#3f454e',
+      },
+      dark: {
+        bg: '#2f2a20',
+        primary: '#74482d',
+        secondary: '#2b8ebb',
+        accent: '#fda229',
+        text: '#c4c4c4',
+      },
+    },
+  },
 });
