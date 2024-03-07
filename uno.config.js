8b2692800c28c93b2153eff79d41cb7810a469d0
Alan-Kuan <kuanhaoan@gmail.com>
2024-03-07 21:36:24 +0800

    chore: add mdi icons

---
+++ uno.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'unocss';
+import presetIcons from '@unocss/preset-icons';
+import presetUno from '@unocss/preset-uno';
+
+export default defineConfig({
+  presets: [
+    presetIcons(),
+    presetUno(),
+  ],
+});
