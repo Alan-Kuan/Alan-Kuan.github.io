f2c3f627ac2fc7a4a7cf164b95b6e2e8357a0840
Alan-Kuan <kuanhaoan@gmail.com>
2024-03-11 11:49:49 +0800

    feat: article list

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  title: String,
+  excerpt: String,
+});
+</script>
+
+<template>
+  <div>
+    <div class="text-xl font-bold">{{ title }}</div>
+    <p class="pt-2">{{ excerpt }}</p>
+  </div>
+</template>
---
+++ src/components/ArticleList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import ArticleCard from './ArticleCard.vue';
+
+defineProps({
+  articles: Array
+});
+</script>
+
+<template>
+  <div class="px-8 py-3">
+    <ArticleCard
+      v-for="article in articles"
+      class="
+        my-5 px-8 py-5
+        rounded-xl
+        bg-#ffffff99
+        drop-shadow hover:drop-shadow-lg
+        cursor-pointer
+      "
+      :title="article.title"
+      :excerpt="article.excerpt"
+    />
+  </div>
+</template>
