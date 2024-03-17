6750cc80a67286c52075c5c16cb69c7a21f77170
Alan-Kuan <kuanhaoan@gmail.com>
2024-03-18 00:03:47 +0800

    fix: should not limit profile card's height

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -7,7 +7,7 @@ defineProps<Profile>();
 <template>
   <div class="
     flex flex-col items-center
-    w-1/3 h-screen pa-5
+    w-1/3 pa-5
     bg-light-bg-profile_card dark:bg-dark-bg-profile_card"
   >
     <img
