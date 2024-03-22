34816f447dbdd4783f5c7f2b43df2fbc7a3f50aa
Alan-Kuan <kuanhaoan@gmail.com>
2024-03-23 01:03:23 +0800

    feat: responsive current route indicator

--- src/components/nav/NavList.vue
+++ src/components/nav/NavList.vue
@@ -1,38 +1,91 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, reactive, onMounted } from 'vue';
 import type { Link } from '@/types.ts';
 
-defineProps<{
+const props = defineProps<{
   links: Link[],
 }>();
 
 const route = ref('');
+var curr_idx = -1;
+
+const link_refs = ref([]);
+var link_widths = [];
+
+const indicator_style = reactive({
+  width: null,
+  transform: null
+});
 
 onMounted(() => {
   route.value = window.location.pathname;
+
+  for (let i = 0; i < props.links.length; i++) {
+    if (props.links[i].route === route.value) {
+      curr_idx = i;
+      break;
+    }
+  }
+
+  for (let i = 0; i < link_refs.value.length; i++) {
+    link_widths[i] = link_refs.value[i].clientWidth;
+  }
+
+  indicator_style.width = `${link_widths[curr_idx] * 0.8}px`;
 });
+
+function onMouseOverLink(target_idx: number) {
+  let trans = 0;
+
+  if (target_idx < curr_idx) {
+    for (let i = curr_idx; i > target_idx; i--) {
+      trans -= 0.5 * link_widths[i] + 0.5 * link_widths[i - 1];
+    }
+  } else {
+    for (let i = curr_idx; i < target_idx; i++) {
+      trans += 0.5 * link_widths[i] + 0.5 * link_widths[i + 1];
+    }
+  }
+
+  indicator_style.width = `${link_widths[target_idx] * 0.8}px`;
+  indicator_style.transform = `translateX(${trans}px)`;
+}
+
+function onMouseLeaveLink() {
+  indicator_style.width = `${link_widths[curr_idx] * 0.8}px`;
+  indicator_style.transform = `translateX(0px)`;
+}
 </script>
 
 <template>
   <div
-    v-for="link in links"
+    v-for="link, idx in links"
     class="relative flex flex-col items-center"
+    ref="link_refs"
   >
     <a
       class="
-      mx-3 px-2 py-0.5
-      text-xl text-#ffeecd hover:bg-#9e775f
-      font-medium
-      rounded
+        mx-3 px-2 py-0.5
+        text-xl text-#ffeecd hover:bg-#9e775f
+        font-medium
+        rounded
       "
       :href="link.route"
+      @mouseover="onMouseOverLink(idx)"
+      @mouseleave="onMouseLeaveLink"
     >
       {{ link.text }}
     </a>
     <!-- current route indicator -->
     <div
       v-if="link.route === route"
-      class="absolute bottom--2 w-80% h-1 bg-#85c0da rounded"
+      class="
+        absolute bottom--2
+        h-1
+        bg-#85c0da rounded
+        transition-all duration-500
+      "
+      :style="indicator_style"
     >
     </div>
   </div>
