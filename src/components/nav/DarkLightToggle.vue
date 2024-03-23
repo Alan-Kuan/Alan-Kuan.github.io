98e55a853eb4a6fe3af087fdeb1a58c9fbb05ce1
Alan-Kuan <kuanhaoan@gmail.com>
2024-03-23 13:00:41 +0800

    feat: add dark/light toggle

---
+++ src/components/nav/DarkLightToggle.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const light_mode = ref(true);
+
+function onClick() {
+  light_mode.value = !light_mode.value;
+
+  if (light_mode.value) {
+    document.body.classList.remove('dark');
+  } else {
+    document.body.classList.add('dark');
+  }
+}
+</script>
+
+<template>
+  <button
+    class="text-#ffeecd text-3xl"
+    @click="onClick"
+  >
+    <div
+      v-if="light_mode"
+      class="i-mdi-white-balance-sunny"
+    >
+    </div>
+    <div
+      v-else
+      class="i-mdi-weather-night"
+    >
+    </div>
+  </button>
+</template>
