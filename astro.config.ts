a5108e2cb787cceb17969e0f5e1bbf4e21f9cf76
Alan-Kuan <kuanhaoan@gmail.com>
2024-06-27 11:40:39 +0800

    feat: update astro and add mdx

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,20 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
-
 import vue from '@astrojs/vue';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    UnoCSS({ injectReset: true }),
     vue(),
-    UnoCSS({
-      injectReset: true,
-    }),
+    mdx(),
   ],
   site: 'https://Alan-Kuan.github.io',
   redirects: {
     '/': '/articles/1',
     '/articles': '/articles/1',
     '/projects': '/projects/1',
-    '/tags/[tag]': '/tags/[tag]/1',
-  },
+    '/tags/[tag]': '/tags/[tag]/1'
+  }
 });
