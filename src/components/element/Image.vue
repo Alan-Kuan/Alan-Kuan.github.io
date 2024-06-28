36a41a128b6f15d50cd3099314cb822bebc97e71
Alan-Kuan <kuanhaoan@gmail.com>
2024-06-28 11:16:00 +0800

    feat: add custom image component

---
+++ src/components/element/Image.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  src: string,
+  alt: string,
+  caption?: string,
+}>();
+</script>
+
+<template>
+  <div my-4>
+    <img
+      rounded-md
+      :src :alt
+      decoding="async" loading="lazy"
+    />
+    <div v-if="caption" mt-2 text="center sm">
+      {{ caption }}
+    </div>
+  </div>
+</template>
