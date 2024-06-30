ef0810666c3ff39c14dc8a1506ca57f19f88ebc8
Alan-Kuan <kuanhaoan@gmail.com>
2024-06-30 19:07:59 +0800

    feat: update style of post attributes

--- src/components/post/PostAttr.vue
+++ src/components/post/PostAttr.vue
@@ -6,37 +6,21 @@ defineProps<{
 </script>
 
 <template>
-  <div flex>
-    <!-- Date -->
-    <div
-      v-if="date"
-      flex items-center
-      mr-5
+  <div v-if="date" font-thin>{{ date }}</div>
+  <div
+    v-if="tags.length > 0"
+    flex items-center
+  >
+    <a
+      v-for="tag in tags"
+      :href="`/tags/${tag}/1`"
+      pa-1 mr-2
+      rounded
+      border="~ link-light dark:text-light"
+      bg="hover:link-light dark:hover:text-light"
+      un-text="link-light dark:text-light hover:white dark:hover:text-dark"
     >
-      <div
-        i-mdi-calendar
-        text="lg link-dark dark:text-light"
-      ></div>
-      <div pl-2>{{ date }}</div>
-    </div>
-
-    <!-- Tags -->
-    <div
-      v-if="tags.length > 0"
-      flex items-center
-    >
-      <div
-        i-mdi-tag
-        text="lg link-dark dark:text-light"
-      ></div>
-      <a
-        v-for="tag in tags"
-        :href="`/tags/${tag}/1`"
-        un-text="link-light dark:text-light"
-        hover:underline
-      >
-        <span pl-2>{{ tag }}</span>
-      </a>
-    </div>
+      <span>#{{ tag }}</span>
+    </a>
   </div>
 </template>
