1c7e6225dbecaaf45d66f360d20e7aa1e2023ab5
Alan-Kuan <kuanhaoan@gmail.com>
2024-06-30 15:06:13 +0800

    refactor: simplify GitHub repo card dark/light mode's displaying method

--- src/components/element/GitHubCard.vue
+++ src/components/element/GitHubCard.vue
@@ -12,36 +12,23 @@ const img_url = `https://github-readme-stats-alan-kuan.vercel.app/api/pin/?usern
 
 <template>
   <a :href="repo_url" target="_blank">
+    <!-- light -->
     <Image
-      class="light-ver"
       height="112px"
       :src="img_url"
       :alt="repo"
+      dark:hidden
       mb-none
       rounded-none
     />
+    <!-- dark -->
     <Image
-      class="dark-ver"
       height="112px"
       :src="`${img_url}&theme=noctis_minimus`"
       :alt="repo"
+      hidden! dark:flex!
       mb-none
       rounded-none
     />
   </a>
 </template>
-
-<style lang="scss" scoped>
-.dark-ver {
-  display: none;
-}
-
-.dark {
-  .light-ver {
-    display: none;
-  }
-  .dark-ver {
-    display: block;
-  }
-}
-</style>
