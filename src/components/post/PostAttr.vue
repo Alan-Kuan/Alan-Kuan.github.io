f7f0c8cb8bcc2445a8a0eb87b90b5655522e2049
Alan-Kuan <kuanhaoan@gmail.com>
2024-06-30 20:30:09 +0800

    fix: post attr margin

--- src/components/post/PostAttr.vue
+++ src/components/post/PostAttr.vue
@@ -6,21 +6,23 @@ defineProps<{
 </script>
 
 <template>
-  <div v-if="date" font-thin>{{ date }}</div>
-  <div
-    v-if="tags.length > 0"
-    flex items-center
-  >
-    <a
-      v-for="tag in tags"
-      :href="`/tags/${tag}/1`"
-      pa-1 mr-2
-      rounded
-      border="~ link-light dark:text-light"
-      bg="hover:link-light dark:hover:text-light"
-      un-text="link-light dark:text-light hover:white dark:hover:text-dark"
+  <div>
+    <div v-if="date" font-thin>{{ date }}</div>
+    <div
+      v-if="tags.length > 0"
+      flex items-center
     >
-      <span>#{{ tag }}</span>
-    </a>
+      <a
+        v-for="tag in tags"
+        :href="`/tags/${tag}/1`"
+        pa-1 mr-2
+        rounded
+        border="~ link-light dark:text-light"
+        bg="hover:link-light dark:hover:text-light"
+        un-text="link-light dark:text-light hover:white dark:hover:text-dark"
+      >
+        <span>#{{ tag }}</span>
+      </a>
+    </div>
   </div>
 </template>
