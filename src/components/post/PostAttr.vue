584a81cd8dee7b1814cdd5fb9e44a7e9595a4bda
Alan-Kuan <kuanhaoan@gmail.com>
2024-07-14 23:31:57 +0800

    feat: make post card responsive

--- src/components/post/PostAttr.vue
+++ src/components/post/PostAttr.vue
@@ -10,12 +10,12 @@ defineProps<{
     <div v-if="date" font-thin>{{ date }}</div>
     <div
       v-if="tags.length > 0"
-      flex items-center
+      flex="~ wrap" items-center
     >
       <a
         v-for="tag in tags"
         :href="`/tags/${tag}/1`"
-        pa-1 mr-2
+        pa-1 mt-2 mr-2
         rounded
         border="~ link-light dark:text-light"
         bg="hover:link-light dark:hover:text-light"
