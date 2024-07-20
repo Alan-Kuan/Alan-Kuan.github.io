eb481f8c8c8dd9a5ecef35931630dd663c66f6a0
Alan-Kuan <kuanhaoan@gmail.com>
2024-07-20 12:31:42 +0800

    style: update interface delimiters

--- src/types.ts
+++ src/types.ts
@@ -1,48 +1,48 @@
 export interface Link {
-  url: string,
-  title: string,
-  icon?: string,
-};
+  url: string;
+  title: string;
+  icon?: string;
+}
 
 export interface Profile {
   avatar_urls: {
     light: string,
     dark: string,
   },
-  name: string,
-  title: string,
+  name: string;
+  title: string;
   org: {
     text: string,
     url: string,
-  },
-  links: Link[],
+  };
+  links: Link[];
   email: {
     addr_b64: string,
     title: string,
-  },
-};
+  };
+}
 
 export interface Post extends PostData {
-  collection: string,
-  slug: string,
-  excerpt?: string,
-};
+  collection: string;
+  slug: string;
+  excerpt?: string;
+}
 
 export interface PostData {
-  title: string,
-  desc: string,
-  date?: string,
-  tags: string[],
-  banner_url?: string,
-};
+  title: string;
+  desc: string;
+  date?: string;
+  tags: string[];
+  banner_url?: string;
+}
 
 export interface ConfPub {
-  title: string,
-  authors: string[],
-  conference: string,
-  location: string,
-  time: string,
-};
+  title: string;
+  authors: string[];
+  conference: string;
+  location: string;
+  time: string;
+}
 
 // NOTE: may add new publication type in the future
 export type Publication = ConfPub;
