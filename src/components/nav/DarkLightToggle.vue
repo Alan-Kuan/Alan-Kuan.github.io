ec5a10f70c03b60a4012ec832c1d5a9e760d9e31
Alan-Kuan <kuanhaoan@gmail.com>
2024-12-14 15:29:10 +0800

    feat: add hover animation to dark/light mode switch button

--- src/components/nav/DarkLightToggle.vue
+++ src/components/nav/DarkLightToggle.vue
@@ -24,20 +24,36 @@ onMounted(() => {
 <template>
   <button
     @click="onClick"
+    relative
     pa-2
     bg="@hover:nav-highlight"
     rounded-full
     text-3xl
+    group
   >
     <div
       v-if="light_mode"
       i-mdi-white-balance-sunny
-    >
-    </div>
-    <div
-      v-else
-      i-mdi-weather-night
-    >
+      group-hover:rotate-180 transition-transform duration-1000
+    />
+    <div v-else>
+      <div
+        i-tabler-star-filled
+        style="font-size: 8px;"
+        absolute top-0 right-3
+        opacity-0
+        group-hover:opacity-100 group-hover:top-2
+        transition-all duration-300
+      />
+      <div
+        i-tabler-star-filled
+        style="font-size: 9px;"
+        absolute top-2 right-1
+        opacity-0
+        group-hover:opacity-100 group-hover:top-4
+        transition-all duration-200
+      />
+      <div i-tabler-moon />
     </div>
   </button>
 </template>
