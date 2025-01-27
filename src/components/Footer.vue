bd08ef1b9730b911b7a56328f48d7f36becd8dbf
Alan-Kuan <kuanhaoan@gmail.com>
2025-01-27 18:30:09 +0800

    feat: replace present with full year

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+const this_year = new Date().getFullYear();
+</script>
+
 <template>
   <footer my-5 text-center>
-    © Alan Kuan 2024 - PRESENT
+    © Alan Kuan 2024 – {{this_year}}
   </footer>
 </template>
