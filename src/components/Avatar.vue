7dd6fa6f0bd48380874da0ce7fc3ba3944f923cb
Alan-Kuan <kuanhaoan@gmail.com>
2025-06-01 00:25:59 +0800

    refactor: make avatar an independent component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Image from '@/components/Image.vue';
+
+defineProps<{
+  avatar_urls: {
+    [key: string]: string,
+  }
+}>();
+</script>
+
+<template>
+  <Image
+    v-for="(url, mode) in avatar_urls"
+    :class="[
+      { 'dark:hidden': mode === 'light' },
+      { 'hidden! dark:flex!': mode === 'dark' },
+      'lt-md:max-w-40', 'lt-md:max-h-40',
+      'w-full', 'h-auto',
+      'sm:w-50', 'sm:h-50',
+      'rounded-xl',
+    ]"
+    :src="url"
+    :alt="`My avatar in ${mode} style`"
+  />
+</template>
