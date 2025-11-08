91df8d3e4ac7c1cfa6fb45cd5b1e011fcb9a2aaf
Alan-Kuan <kuanhaoan@gmail.com>
2025-11-08 23:00:37 +0800

    feat: add callout support

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import vue from '@astrojs/vue';
 import mdx from '@astrojs/mdx';
+import remarkCallout from '@r4ai/remark-callout';
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: { theme: 'ayu-dark' },
+    remarkPlugins: [
+      remarkCallout,
+    ],
   },
   integrations: [
     UnoCSS({ injectReset: true }),
