d0a49aa3e432505bf8def734b2e4c8e755ed187f
Alan-Kuan <kuanhaoan@gmail.com>
2025-11-17 22:39:45 +0800

    fix: missing captions when viewing post images

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue';
 const props = defineProps<{
   src: string,
   alt: string,
+  title?: string,
 }>();
 
 // Set `img_src` on mounted in case that the class list is
@@ -15,7 +16,7 @@ onMounted(() => img_src.value = props.src);
 
 <template>
   <img
-    :src="img_src" :alt
+    :src="img_src" :alt :title
     loading="lazy"
     onload="this.classList.remove('blur-2')"
     onerror="this.classList.remove('text-transparent', 'blur-2')"
