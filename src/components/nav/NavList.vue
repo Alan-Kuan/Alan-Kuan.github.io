d84969393c043d87f405ff33ad1c757bb0141a48
Alan-Kuan <kuanhaoan@gmail.com>
2026-02-08 22:45:05 +0800

    refactor: implement nav list indicator animation with motion

--- src/components/nav/NavList.vue
+++ src/components/nav/NavList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
 import { useStore } from '@nanostores/vue';
+import { motion } from 'motion-v';
+import { ref } from 'vue';
 
 import type { NavLink } from '@/types/content';
 import { show_nav_menu } from '@/stores/NavMenuStore';
@@ -10,79 +11,50 @@ const props = defineProps<{
   links: NavLink[],
 }>();
 
-const link_refs = ref<HTMLAnchorElement[]>([]);
-var link_widths: number[] = [];
 const $show_nav_menu = useStore(show_nav_menu);
-
-const width = ref('');
-const transform = ref('');
-
-onMounted(() => {
-  link_refs.value.forEach((link_ele, i) => {
-    link_widths[i] = link_ele.clientWidth;
-  });
-});
-
-function onMouseOverLink(target_idx: number) {
-  const dir = target_idx < props.curr_idx ? -1 : 1;
-  let offset = 0;
-
-  for (let i = props.curr_idx; i != target_idx; i += dir) {
-    // 24 is the margin (mx-3 -> 3 * 2 * 0.25 * 16 = 24)
-    offset += dir * (0.5 * (link_widths[i] + link_widths[i + dir]) + 24);
-  }
-
-  width.value = `${link_widths[target_idx]}px`;
-  transform.value = `translateX(${offset}px)`;
-}
-
-function onMouseLeaveLink() {
-  width.value = '';
-  transform.value = '';
-}
+const highlighted_idx = ref(props.curr_idx);
 </script>
 
 <template>
   <div
     :class="[
-      'flex', 'justify-center',
-      'transition-all', 'duration-500',
+      'flex justify-center',
+      'transition-all duration-500',
       { 'lt-md:opacity-0 lt-md:invisible': $show_nav_menu }
     ]"
   >
     <div
       v-for="link, idx in links"
-      class="
-        relative
-        flex flex-col items-center
-      "
+      @mouseenter="highlighted_idx = idx"
+      @mouseleave="highlighted_idx = curr_idx"
+      :class="[
+        'relative',
+        'mx-3',
+        'flex flex-col items-center',
+        { 'lt-md:hidden': idx !== highlighted_idx },
+      ]"
     >
       <a
-        ref="link_refs"
         :href="link.route"
-        @mouseover="onMouseOverLink(idx)"
-        @mouseleave="onMouseLeaveLink"
-        :class="[
-          'mx-3', 'px-2', 'py-0.5',
-          'hover:bg-#ffffff55',
-          'text-lg', 'md:text-xl',
-          'rounded-sm',
-          { 'lt-md:hidden': idx !== curr_idx }
-        ]"
+        class="
+          px-2 py-0.5
+          hover:bg-#ffffff55
+          text-lg md:text-xl
+          rounded-sm
+        "
       >
         {{ link.text }}
       </a>
       <!-- current route indicator -->
-      <div
-        v-if="idx === curr_idx"
+      <motion.div
+        v-if="idx === highlighted_idx"
+        layout-id="indicator"
         class="
           absolute bottom--2
-          w-[calc(100%-1.5rem)] h-1
+          w-full h-1
           bg-indicator-info
           rounded-sm
-          transition-[width,transform] duration-500
         "
-        :style="{ width, transform }"
       />
     </div>
   </div>
